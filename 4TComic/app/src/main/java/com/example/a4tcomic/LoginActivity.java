6f12fada01341a88dc22e58483f0e5c0c9116082
TuanHuu168 <tuanhuu.thhn@gmail.com>
2024-05-25 13:14:42 +0700

    Fix crash app, add register activity and add some translation to app

--- 4TComic/app/src/main/java/com/example/a4tcomic/LoginActivity.java
+++ 4TComic/app/src/main/java/com/example/a4tcomic/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.a4tcomic;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Khai báo
+    EditText edtUserName, edtPassword;
+    TextView lblForgot, lblRegister;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginMain), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Ánh xạ
+        edtUserName = findViewById(R.id.edtUserName);
+        edtPassword = findViewById(R.id.edtPassword);
+        lblForgot = findViewById(R.id.lblForgot);
+        lblRegister = findViewById(R.id.lblRegister);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        // Gạch chân
+        lblForgot.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        lblRegister.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
     }
 }
---
+++ 4TComic/app/src/main/java/com/example/a4tcomic/RegisterActivity.java
@@ -0,0 +1,62 @@
+package com.example.a4tcomic;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.UnderlineSpan;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    // Khai báo
+    EditText edtUserName, edtEmail, edtPassword, edtRePassword;
+    TextView lblLogin;
+    RadioButton rd_terms;
+    Button btnRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registerMain), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Ánh xạ
+        edtUserName = findViewById(R.id.edtUserName);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtRePassword = findViewById(R.id.edtRePassword);
+        lblLogin = findViewById(R.id.lblLogin);
+        rd_terms = findViewById(R.id.rd_terms);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        // Đổi màu chữ và gạch chân điều khoản & dịch vụ
+        String fullText = getString(R.string.terms_text);
+        String underlinedText = getString(R.string.terms_text_underline);
+
+        SpannableString spannableString = new SpannableString(fullText);
+
+        int start = fullText.indexOf(underlinedText);
+        int end = start + underlinedText.length();
+
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#FFB3C6")), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new UnderlineSpan(), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        rd_terms.setText(spannableString);
+
+    }
+}
