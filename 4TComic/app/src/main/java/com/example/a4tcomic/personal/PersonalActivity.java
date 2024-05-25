528f1f31ab34aee33d427c19bbbd49d9aa4402c5
W-Thang <nqtfacebookanhxtanh@gmail.com>
2024-05-26 01:39:43 +0700

    update personal

--- 4TComic/app/src/main/java/com/example/a4tcomic/personal/PersonalActivity.java
+++ 4TComic/app/src/main/java/com/example/a4tcomic/personal/PersonalActivity.java
@@ -1,6 +1,10 @@
 package com.example.a4tcomic.personal;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,8 @@ import com.example.a4tcomic.R;
 
 public class PersonalActivity extends AppCompatActivity {
 
+    private TextView tv_edit_profile;
+    private Button btn_account, btn_setting, btn_admin, btn_logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,46 @@ public class PersonalActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Tham chiếu đến Views
+        tv_edit_profile = findViewById(R.id.tv_edit_profile);
+        btn_account = findViewById(R.id.btn_account);
+        btn_setting = findViewById(R.id.btn_setting);
+        btn_admin = findViewById(R.id.btn_admin);
+        btn_logout = findViewById(R.id.btn_logout);
+
+        // Sự kiện
+        tv_edit_profile.setOnClickListener(v -> {
+            // Xử lý sự kiện khi TextView được nhấp vào
+        });
+
+        btn_account.setOnClickListener(v -> {
+            // Xử lý sự kiện khi Button được nhấp vào
+        });
+
+        btn_setting.setOnClickListener(v -> {
+            // Xử lý sự kiện khi Button được nhấp vào
+        });
+
+        btn_admin.setOnClickListener(v -> {
+            // Xử lý sự kiện khi Button được nhấp vào
+        });
+
+        btn_logout.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.title_dialog_logout);
+            builder.setMessage(R.string.message_logout);
+
+            // truyền màu cho text
+            builder.setPositiveButton(R.string.string_no, (dialog, which) -> {
+                dialog.dismiss();
+            });
+            builder.setNeutralButton(R.string.string_yes, (dialog, which) -> {
+                this.finish();
+            });
+            Dialog dialog = builder.create();
+            dialog.show();
         });
     }
 }
