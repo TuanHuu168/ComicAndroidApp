1773be9965ec7cf73414030d78917acddc2275c1
W-Thang <nqtfacebookanhxtanh@gmail.com>
2024-05-25 22:42:48 +0700

    update story_detail_activity

---
+++ 4TComic/app/src/main/java/com/example/a4tcomic/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.a4tcomic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.a4tcomic.story_detail.StoryDetailActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, StoryDetailActivity.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ 4TComic/app/src/main/java/com/example/a4tcomic/story_detail/Chapter.java
@@ -0,0 +1,37 @@
+package com.example.a4tcomic.story_detail;
+
+public class Chapter {
+    private int resourceId;
+    private String name;
+    private String time_update;
+
+    public Chapter(int resourceId, String name, String time_update) {
+        this.resourceId = resourceId;
+        this.name = name;
+        this.time_update = time_update;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTimeUpdate() {
+        return time_update;
+    }
+
+    public void setTimeUpdate(String time_update) {
+        this.time_update = time_update;
+    }
+}
