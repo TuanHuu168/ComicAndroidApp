19b0920dcaa315ef68e2767383eeb08fe340cd97
anhtuyet2k3 <luyenthianht@gmail.com>
2024-05-29 00:27:46 +0700

    Update UI code for bookcase_page and chapter_page

--- 4TComic/app/src/main/java/com/example/a4tcomic/BookAdapter.java
+++ 4TComic/app/src/main/java/com/example/a4tcomic/BookAdapter.java
@@ -4,7 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -13,47 +13,56 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class BookAdapter extends RecyclerView.Adapter<BookAdapter.AppViewHolder>{
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.AppViewHolder> {
     private List<ListBookCase> list;
     private boolean isEditMode = false;
+    private boolean showReadingChapter;
 
-    public void setEditMode(boolean isEditMode){
+    public BookAdapter(List<ListBookCase> list, boolean showReadingChapter) {
+        this.list = list;
+        this.showReadingChapter = showReadingChapter;
+    }
+
+    public void setEditMode(boolean isEditMode) {
         this.isEditMode = isEditMode;
         notifyDataSetChanged(); // cập nhật lại recyclerView
     }
-    public void selectAll(){
-        for(ListBookCase book : list){
+
+    public void selectAll() {
+        for (ListBookCase book : list) {
             book.setChecked(true);
         }
-        notifyDataSetChanged(); // cập nhật lại revycler view để hiển thị trang thái đã chọn
+        notifyDataSetChanged(); // cập nhật lại recycler view để hiển thị trạng thái đã chọn
     }
-    public void deselectAll(){
-        for(ListBookCase book : list){
+
+    public void deselectAll() {
+        for (ListBookCase book : list) {
             book.setChecked(false);
         }
-        notifyDataSetChanged(); // cập nhật lại revycler view để hiển thị trang thái bỏ chọn
+        notifyDataSetChanged(); // cập nhật lại recycler view để hiển thị trạng thái bỏ chọn
     }
-    public BookAdapter(List<ListBookCase> list) {
-        this.list = list;
+
+    public void setShowReadingChapter(boolean showReadingChapter) {
+        this.showReadingChapter = showReadingChapter;
+        notifyDataSetChanged(); // cập nhật lại recyclerView
     }
 
     @NonNull
     @Override
     public AppViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.item_bookcase,parent,false);
+        View view = layoutInflater.inflate(R.layout.item_bookcase, parent, false);
         return new AppViewHolder(view);
     }
 
-    // Xử lý su kiện radio button chọn và không chọn
     @Override
     public void onBindViewHolder(@NonNull AppViewHolder holder, int position) {
         ListBookCase listBookCase = list.get(position);
-        if(listBookCase!= null){
+        if (listBookCase != null) {
             holder.imgBtnItem.setImageResource(listBookCase.getImageBook());
             holder.tvName.setText(listBookCase.getNameBook());
             holder.radioButton.setChecked(listBookCase.isChecked());
-            holder.radioButton.setVisibility(isEditMode?View.VISIBLE:View.GONE);
+            holder.radioButton.setVisibility(isEditMode ? View.VISIBLE : View.INVISIBLE);
             holder.radioButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -61,6 +70,7 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.AppViewHolder>
                     holder.radioButton.setChecked(listBookCase.isChecked());
                 }
             });
+            holder.lyReadingChapter.setVisibility(showReadingChapter ? View.VISIBLE : View.INVISIBLE);
         }
     }
 
@@ -69,15 +79,18 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.AppViewHolder>
         return list.size();
     }
 
-    public class AppViewHolder extends RecyclerView.ViewHolder{
+    public class AppViewHolder extends RecyclerView.ViewHolder {
         ImageButton imgBtnItem;
         TextView tvName;
         RadioButton radioButton;
+        LinearLayout lyReadingChapter;
+
         public AppViewHolder(@NonNull View itemView) {
             super(itemView);
-            imgBtnItem  = itemView.findViewById(R.id.imgBtnItem);
+            imgBtnItem = itemView.findViewById(R.id.imgBtnItem);
             tvName = itemView.findViewById(R.id.titleItem);
             radioButton = itemView.findViewById(R.id.radio_button_book);
+            lyReadingChapter = itemView.findViewById(R.id.lyReadingChapter);
         }
     }
 }
