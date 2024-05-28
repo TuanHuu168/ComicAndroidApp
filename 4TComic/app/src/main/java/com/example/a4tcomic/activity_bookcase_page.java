9bf015f89c0559d52ffa43c40dc014913733897b
anhtuyet2k3 <luyenthianht@gmail.com>
2024-05-29 00:27:46 +0700

    Update UI code for bookcase_page and chapter_page

--- 4TComic/app/src/main/java/com/example/a4tcomic/activity_bookcase_page.java
+++ 4TComic/app/src/main/java/com/example/a4tcomic/activity_bookcase_page.java
@@ -1,46 +1,75 @@
 package com.example.a4tcomic;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 import java.util.List;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat.Type;
 
-public class activity_follow_page extends AppCompatActivity {
 
-    ImageButton btnEdit,btnBack;
-    Button btnHistory, btnChooseAll;
 
-    RecyclerView rcvFollow;
+public class activity_bookcase_page extends AppCompatActivity {
+    ImageButton btnBack, btnEdit;
+    Button btnFollow, btnChooseAll,btnHistory;
+    RecyclerView rcvHistory;
     BookAdapter bookAdapter;
     LinearLayout llayout;
     boolean isEditButtonColored = false; // Trạng thái ban đầu của nút
-    boolean isAllSelected = false; // Trạng thái ban đầu của tất cả các nút
+    boolean isAllSelected = false; // Trạng thái ban đầu của nút
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_follow_page);
+        setContentView(R.layout.activity_bookcase_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, ((Insets) systemBars).bottom);
+            return WindowInsetsCompat.CONSUMED;
+        });
 
-        btnBack = findViewById(R.id.btnBack);
-        btnHistory = findViewById(R.id.btnHistory);
+
+        btnFollow = findViewById(R.id.btnFollow);
         btnEdit = findViewById(R.id.btnEdit);
         llayout = findViewById(R.id.llayout);
         llayout.setVisibility(View.GONE);
         btnChooseAll = findViewById(R.id.btnChooseAll);
+        btnHistory = findViewById(R.id.btnHistory);
 
+        // Set initial state
+        btnHistory.setBackgroundResource(R.drawable.ic_bg_blue);
+        btnFollow.setBackgroundResource(R.drawable.ic_bg_white);
+
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bookAdapter.setShowReadingChapter(true);
+                btnHistory.setBackgroundResource(R.drawable.ic_bg_blue);
+                btnFollow.setBackgroundResource(R.drawable.ic_bg_white);
+                bookAdapter.notifyDataSetChanged(); // Cập nhật lại RecyclerView
+            }
+        });
+
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bookAdapter.setShowReadingChapter(false);
+                btnFollow.setBackgroundResource(R.drawable.ic_bg_blue);
+                btnHistory.setBackgroundResource(R.drawable.ic_bg_white);
+                bookAdapter.notifyDataSetChanged(); // Cập nhật lại RecyclerView
+            }
+        });
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,14 +84,6 @@ public class activity_follow_page extends AppCompatActivity {
                     bookAdapter.setEditMode(true);
                 }
                 isEditButtonColored = !isEditButtonColored; // Đổi trạng thái
-            }
-
-        });
-        btnHistory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentHistory = new Intent(activity_follow_page.this, activity_history_page.class);
-                startActivity(intentHistory);
             }
         });
 
@@ -79,14 +100,25 @@ public class activity_follow_page extends AppCompatActivity {
                 isAllSelected=!isAllSelected; // đổi trạng thái
             }
         });
+//        btnFollow.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intentFollow = new Intent(activity_bookcase_page.this, activity_follow_page.class);
+//                startActivity(intentFollow);
+//            }
+//        });
 
-        rcvFollow = findViewById(R.id.rcvFollow);
+        rcvHistory = findViewById(R.id.rcvHistory);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
-        rcvFollow.setLayoutManager(linearLayoutManager);
-        bookAdapter = new BookAdapter(getList());
-        rcvFollow.setAdapter(bookAdapter);
+        rcvHistory.setLayoutManager(linearLayoutManager);
+        bookAdapter = new BookAdapter(getList(),false);
+        rcvHistory.setAdapter(bookAdapter);
 
+        // Set initial visibility state
+        bookAdapter.setShowReadingChapter(true);
+        bookAdapter.notifyDataSetChanged(); // Cập nhật lại RecyclerView
     }
+
     private List<ListBookCase> getList() {
         List<ListBookCase> list = new ArrayList<>();
         list.add(new ListBookCase(R.drawable.tinhtukiemsi, "Tinh tú kiếm sĩ"));
